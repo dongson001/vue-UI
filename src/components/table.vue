b3eb27b88ae907e88b8d348a20bc10d8968fe02c
liudong <825826837@qq.com>
2022-11-04 17:33:30 +0800

    init

---
+++ src/components/table.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-table :data="tableOption.data" :border="tableOption.border || true" :stripe="tableOption.stripe || true" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const props = defineProps({
+  tableOption: {
+    type: Object,
+    required: true,
+  },
+});
+
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/view/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <Table :tableOption="stateTable"></Table>
+</template>
+
+<script setup>
+import Table from '../components/table.vue';
+import { reactive } from 'vue';
+let stateTable = {
+  data: [
+    {
+      date: '2016-05-03',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-04',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+      date: '2016-05-01',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles',
+    },
+  ],
+};
+</script>
+
+<style lang="scss" scoped></style>
